d8a5022e75dd5baad173b0bdffeb9cb42f6385ad
Eun Oh <chorockmasil@gmail.com>
2023-06-23 00:25:23 +0900

    230623 eun svelte workerFunctions2

--- functions/artfunction.js
+++ functions/artfunction.js
@@ -1,9 +1,8 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
+// addEventListener('fetch', event => {
+//     event.respondWith(handleRequest(event.request))
+// })
 
-export async function handleRequest(request) {
-    let url = new URL(request.url);
+export async function onRequest(request) {
 
     if (request.method === 'POST') {
         const requestBody = await request.json(); // 본문을 JSON으로 파싱
@@ -11,9 +10,11 @@ export async function handleRequest(request) {
         return new Response(`Data saved!`, {status: 200});
 
     } else if (request.method === 'GET') {
+        let url = new URL(request.url);
         let key = url.searchParams.get("key");
         let store = await art_history.get(key);
-        return new Response(store || '', {status: 200})
+        return new Response(store || '', {status: 200});
+        
     } else {
         return new Response(`Method not supported.`, {status: 405})
     }
